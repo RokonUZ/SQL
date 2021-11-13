8d82e8b3091d72384a35a0044a9cb75001237a42
RokonUZ <88274599+RokonUZ@users.noreply.github.com>
2021-11-13 10:26:17 +0600

    Create practice1.sql

---
+++ practice1.sql
@@ -0,0 +1,7 @@
+create table class7(Roll integer , Name varchar(20) );
+
+insert into  class7 values(1, "roman");
+insert into  class7 values(2, "amir");
+insert into  class7 values(3, "rahat");
+
+select * from class7;
